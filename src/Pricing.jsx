cfc3aa7c027b8c47767fa5cc515fc79177bfaa2c
Jonathan <jonathan-516@hotmail.com>
2024-01-30 17:53:25 -0300

    update

--- src/Pricing.jsx
+++ src/Pricing.jsx
@@ -1,61 +1,64 @@
-import React, { useState } from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardHeader from '@mui/material/CardHeader';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import StarIcon from '@mui/icons-material/StarBorder';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-import GlobalStyles from '@mui/material/GlobalStyles';
-import Container from '@mui/material/Container';
-import MobileStepper from '@mui/material/MobileStepper';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import React, { useState } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardHeader from "@mui/material/CardHeader";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import StarIcon from "@mui/icons-material/StarBorder";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import Container from "@mui/material/Container";
+import MobileStepper from "@mui/material/MobileStepper";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 
 const tiers = [
   {
-    title: 'Free',
-    price: '0',
+    title: "Free",
+    price: "0",
     description: [
-      '10 users included',
-      '2 GB of storage',
-      'Help center access',
-      'Email support',
+      "10 users included",
+      "2 GB of storage",
+      "Help center access",
+      "Email support",
     ],
-    buttonText: 'Sign up for free',
-    buttonVariant: 'outlined',
+    buttonText: "Sign up for free",
+    buttonVariant: "outlined",
   },
   {
-    title: 'Pro',
-    subheader: 'Most popular',
-    price: '15',
+    title: "Pro",
+    subheader: "Most popular",
+    price: "15",
     description: [
-      '20 users included',
-      '10 GB of storage',
-      'Help center access',
-      'Priority email support',
+      "20 users included",
+      "10 GB of storage",
+      "Help center access",
+      "Priority email support",
     ],
-    buttonText: 'Get started',
-    buttonVariant: 'contained',
+    buttonText: "Get started",
+    buttonVariant: "contained",
   },
   {
-    title: 'Enterprise',
-    price: '30',
+    title: "Enterprise",
+    price: "30",
     description: [
-      '50 users included',
-      '30 GB of storage',
-      'Help center access',
-      'Phone & email support',
+      "50 users included",
+      "30 GB of storage",
+      "Help center access",
+      "Phone & email support",
     ],
-    buttonText: 'Contact us',
-    buttonVariant: 'outlined',
+    buttonText: "Contact us",
+    buttonVariant: "outlined",
   },
 ];
 
@@ -63,9 +66,16 @@ const tiers = [
 const defaultTheme = createTheme();
 
 export default function Pricing() {
-    const theme = useTheme();
-    const [coins, setCoins] = useState(0);
-    const [activeStep, setActiveStep] = React.useState(0);
+    const settings = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3, // Número de cards a serem exibidos ao mesmo tempo
+        slidesToScroll: 1,
+      };
+
+  const theme = useTheme();
+  const [coins, setCoins] = useState(0);
+  const [activeStep, setActiveStep] = React.useState(0);
 
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -77,122 +87,122 @@ export default function Pricing() {
     setCoins(coins - 1);
   };
 
-    
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+      <GlobalStyles
+        styles={{ ul: { margin: 0, padding: 0, listStyle: "none" } }}
+      />
       <CssBaseline />
       {/* Hero unit */}
-      <Container disableGutters maxWidth="sm" component="main" sx={{ pt: 8, pb: 6 }}>
+      <Container
+        disableGutters
+        maxWidth="sm"
+        component="main"
+        sx={{ pt: 8, pb: 6 }}
+      >
         <Typography
           component="h1"
           variant="h2"
           align="center"
           color="text.primary"
           gutterBottom
-          fontFamily='Silkscreen, sans-serif'
-          border='solid 1px'
+          fontFamily="Silkscreen, sans-serif"
+          border="solid 1px"
         >
           INCERT COINS: {coins}
         </Typography>
-        <Typography variant="h5" align="center" color="text.secondary" component="p">
-          Quickly build an effective pricing table for your potential customers with
-          this layout. It&apos;s built with default MUI components with little
-          customization.
+        <Typography
+          variant="h5"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Quickly build an effective pricing table for your potential customers
+          with this layout. It&apos;s built with default MUI components with
+          little customization.
         </Typography>
       </Container>
       {/* End hero unit */}
       <Container maxWidth="md" component="main">
-        <Grid container spacing={5} alignItems="flex-end">
-          {tiers.map((tier) => (
-            // Enterprise card is full width at sm breakpoint
-            <Grid
-              item
-              key={tier.title}
-              xs={12}
-              sm={tier.title === 'Enterprise' ? 12 : 6}
-              md={4}
-            >
-              <Card>
-                <CardHeader
-                  title={tier.title}
-                  subheader={tier.subheader}
-                  titleTypographyProps={{ align: 'center' }}
-                  action={tier.title === 'Pro' ? <StarIcon /> : null}
-                  subheaderTypographyProps={{
-                    align: 'center',
-                  }}
-                  sx={{
-                    backgroundColor: (theme) =>
-                      theme.palette.mode === 'light'
-                        ? theme.palette.grey[200]
-                        : theme.palette.grey[700],
-                  }}
-                />
-                <CardContent>
-                  <Box
-                    sx={{
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'baseline',
-                      mb: 2,
-                    }}
+      <Slider {...settings}>
+      {tiers.map((tier) => (
+        <div key={tier.title} style={{ display: 'flex', justifyContent: 'center' }}>
+          <Card sx={{ maxWidth: 345, minWidth: 250, width: '100%' }}>
+            <CardHeader
+              title={tier.title}
+              subheader={tier.subheader}
+              titleTypographyProps={{ align: 'center' }}
+              action={tier.title === 'Pro' ? <StarIcon /> : null}
+              subheaderTypographyProps={{ align: 'center' }}
+              sx={{
+                backgroundColor: (theme) =>
+                  theme.palette.mode === 'light'
+                    ? theme.palette.grey[200]
+                    : theme.palette.grey[700],
+              }}
+            />
+            <CardContent>
+              <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'baseline', mb: 2 }}>
+                <Typography component="h2" variant="h3" color="text.primary">
+                  ${tier.price}
+                </Typography>
+                <Typography variant="h6" color="text.secondary">
+                  /mo
+                </Typography>
+              </Box>
+              <ul>
+                {tier.description.map((line) => (
+                  <Typography
+                    component="li"
+                    variant="subtitle1"
+                    align="center"
+                    key={line}
                   >
-                    <Typography component="h2" variant="h3" color="text.primary">
-                      ${tier.price}
-                    </Typography>
-                    <Typography variant="h6" color="text.secondary">
-                      /mo
-                    </Typography>
-                  </Box>
-                  <ul>
-                    {tier.description.map((line) => (
-                      <Typography
-                        component="li"
-                        variant="subtitle1"
-                        align="center"
-                        key={line}
-                      >
-                        {line}
-                      </Typography>
-                    ))}
-                  </ul>
-                </CardContent>
-                <CardActions>
-                  
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
+                    {line}
+                  </Typography>
+                ))}
+              </ul>
+            </CardContent>
+          </Card>
+        </div>
+      ))}
+    </Slider>
+
         <MobileStepper
-      variant="progress"
-      steps={100}
-      position="static"
-      activeStep={activeStep}
-      sx={{ maxWidth: 700, flexGrow: 1 }}
-      
-      nextButton={
-        <Button size="small" onClick={handleNext} disabled={activeStep === 100}>
-          Next
-          {theme.direction === 'rtl' ? (
-            <KeyboardArrowLeft />
-          ) : (
-            <KeyboardArrowRight />
-          )}
-        </Button>
-      }
-      backButton={
-        <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-          {theme.direction === 'rtl' ? (
-            <KeyboardArrowRight />
-          ) : (
-            <KeyboardArrowLeft />
-          )}
-          Back
-        </Button>
-      }
-    />
+          variant="progress"
+          steps={100}
+          position="static"
+          activeStep={activeStep}
+          sx={{ maxWidth: 700, flexGrow: 1 }}
+          nextButton={
+            <Button
+              size="small"
+              onClick={handleNext}
+              disabled={activeStep === 100}
+            >
+              Next
+              {theme.direction === "rtl" ? (
+                <KeyboardArrowLeft />
+              ) : (
+                <KeyboardArrowRight />
+              )}
+            </Button>
+          }
+          backButton={
+            <Button
+              size="small"
+              onClick={handleBack}
+              disabled={activeStep === 0}
+            >
+              {theme.direction === "rtl" ? (
+                <KeyboardArrowRight />
+              ) : (
+                <KeyboardArrowLeft />
+              )}
+              Back
+            </Button>
+          }
+        />
       </Container>
     </ThemeProvider>
   );
