46b9876aeb3ef555ef2ce8fb24dae9aeca342d91
Jonathan <jonathan-516@hotmail.com>
2024-01-30 16:53:43 -0300

    update

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import InsertCoin from './InsertCoin'
+import Pricing from './Pricing'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 export default function() {
   return (
     <Router>
         <Routes>
-            <Route path="/" element={<InsertCoin/>}></Route>
+            {/* <Route path="/" element={<InsertCoin/>}></Route> */}
+            <Route path="/" element={<Pricing/>}></Route>
         </Routes>
     </Router>
   )
