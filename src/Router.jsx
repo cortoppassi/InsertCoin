b61942d8da22f9b0af7cb945f25a0ecfdb9e1f9b
Jonathan <jonathan-516@hotmail.com>
2024-02-01 11:04:58 -0300

    update

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Pricing from './Pricing'
+import Pricing from './pages/insertCoins/Pricing'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 export default function() {
